d8e7aec2b1903cf51385d268b5d82b209a69e445
EmersonUrielVargas <emrvargaitaz@gmail.com>
2025-02-26 13:37:22 -0500

    add cors

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,7 +1,9 @@
 import express, { Application } from "express";
 import morgan from "morgan";
+import cors from "cors";
 
 const app: Application = express();
+app.use(cors())
 app.use(express.json());
 app.use(morgan("dev"));
 
